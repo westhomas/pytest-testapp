6fcc701af1acb4ca72551e34325b90de6e681810
Wes Thomas <westhomas@gmail.com>
2017-05-03 16:26:53 -0500

    Test app for pytest testing.

---
+++ test.py
@@ -0,0 +1,63 @@
+import unittest
+import pytest
+from pytest import mark
+
+
+# Tests
+def test_foo(info):
+    print("test_foo")
+    assert True
+
+def test_bar():
+    print("test_bar")
+    assert True
+
+class Baz(unittest.TestCase):
+    def test_baz(self):
+        print("test_baz")
+        assert True
+
+
+# def get_baz():
+#     class _Baz(unittest.TestCase):
+#         def test_baz(self):
+#             print("test_baz")
+#             assert True
+#
+#     #test_case_class = type('Baz', (unittest.TestCase,), {})
+#     return mark.usefixtures("info")(_Baz)
+#
+# SuperBaz = get_baz()
+
+
+# Execute Tests
+def main():
+
+
+    class MyPlugin(object):
+        def __init__(self, data):
+
+            self.__name__ = "myplugin"
+            self.data = data
+            self.collected = set()
+
+        def pytest_xdist_node_collection_finished(self, node, ids):
+            self.collected.update(set(ids))
+
+        def pytest_collection_modifyitems(self, items):
+            for item in items:
+                self.collected.update({item.nodeid})
+
+        @pytest.fixture(autouse=True)
+        def info(self, request):
+            test = request.config.pluginmanager.get_plugin('myplugin')
+            print("fixture: ", test.data)
+
+    myplugin = MyPlugin('information')
+
+    pytest.main(["test.py", "-s"], plugins=[myplugin])
+
+    print("Collected:", myplugin.collected)
+
+if __name__ == "__main__":
+    main()
